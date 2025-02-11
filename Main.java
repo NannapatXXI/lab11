3cedd60c79120f453783c613cdace9f7fca04e17
NannapatXXI <nannapatxxi@gmail.com>
2025-02-11 17:46:50 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import lib.Form1;
+import lib.Form2;
+public class Main {
+    public static void main(String[] args) {
+        //new Form1();
+       new Form2();
+    }
+}
---
+++ lib/Form1.java
@@ -0,0 +1,118 @@
+package lib;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+
+public class Form1 extends JFrame implements ActionListener{
+
+    Container cp ;
+    JLabel name , id ;
+    JTextField t1 ,t2,t3 ;
+    JButton b1,b2 ;
+    String Check1;
+    String Check2;
+    public Form1(){
+        Initial();
+        setComponent();
+        Finally();
+       
+    }
+    public void Initial(){
+        cp = this.getContentPane();
+        cp.setLayout(null);
+    }
+    public void setComponent(){
+        id = new JLabel("ID : ");
+        name = new JLabel("Name : ");
+        t1 = new JTextField(20);
+        t2 = new JTextField(20);
+        b1 = new JButton("Add");
+        b2 = new JButton("Clear");  
+        
+        id.setBounds(32, 10, 60, 25);
+        name.setBounds(10, 40, 60, 25);
+        t1.setBounds(60, 10, 120, 25);
+        t2.setBounds(60, 40, 180, 25);
+        b1.setBounds(30, 80, 70, 25);
+        b2.setBounds(120, 80, 70, 25);
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        cp.add(id); cp.add(t1);
+        cp.add(name); cp.add(t2);
+        cp.add(b1) ; cp.add(b2);
+    }
+    public void Finally(){
+        this.setSize(270, 160);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+   
+    public void actionPerformed(ActionEvent e) {
+      if (e.getSource() == b1) {
+          Check1 = t1.getText();
+          Check2 = t2.getText();
+        String error = " ";
+        boolean CheckIDName = false;
+        for (int i = 0; i < Check1.length(); i++) {
+            if (!Character.isDigit(Check1.charAt(i))){
+                CheckIDName = true;
+                error +="Error Because  letter in the ID field";
+                break;
+            }
+        }
+        for (int i = 0; i < Check2.length(); i++) {
+            if (!((Character.isAlphabetic(Check2.charAt(i)) ) || (Check2.charAt(i) == ' '))){
+                CheckIDName = true;
+                error +="Error Because  number in the Name field";
+                break;
+            }
+        }
+        if (CheckIDName) {
+            Popup(error);
+        }else{
+            write(Check1, Check2);
+            Popup("Complete");
+        }
+       
+      }else if (e.getSource() == b2) {
+        t1.setText("");
+        t2.setText("");
+      }
+    }
+    public void write(String id , String name){
+        File fo = null;
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        try {
+            fo = new File("./file/STD.csv");
+            fw = new FileWriter(fo);
+            bw = new BufferedWriter(fw);
+            bw.write(id + "," + name);
+            System.out.println("InCheckfiled");
+            
+        } catch (Exception a) {
+            System.out.println(a);
+        }finally{
+            try {
+                bw.close();fw.close();
+            } catch (Exception a) {
+                System.out.println(a);
+            }
+        }
+    }
+    public void Popup(String s){
+        JDialog d = new JDialog();
+        JLabel i = new JLabel(s);
+        i.setBounds(50, 100, 200,  50);
+        d.getContentPane().add(i);
+        d.pack();
+       
+        d.setSize(270, 160);
+        d.setVisible(true);
+        d.setLocationRelativeTo(null);
+        d.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    }
+
